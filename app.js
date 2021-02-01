e0fd57686cf53dc1f69ad56329188954e63cc7c5
Piccolo <kabiryusufbashir@gmail.com>
2021-02-01 12:27:12 +0100

    Complete Homepage

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/courses', (req, res)=>{
+    res.render('courses');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000');
+});
